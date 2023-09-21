769074944eead390ffff26eef0cd054fb1fc3f94
JungSik Jeong <wndtlr1024@gmail.com>
2023-09-21 16:49:59 +0900

    Feat:스와이프 키보드기능 추가

--- components/PostSwiper.tsx
+++ components/PostSwiper.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper/modules';
+import { Keyboard, Navigation } from 'swiper/modules';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 import { FcLikePlaceholder } from 'react-icons/fc';
 import { BiShareAlt } from 'react-icons/bi';
@@ -25,102 +25,103 @@ const PostSwiper = ({ moveSectionDown }: any) => {
     console.log('클릭');
   };
   return (
-    <div className='w-full h-screen'>
+    <div className='w-screen h-screen bg-black'>
       <Swiper
         slidesPerView={1}
         spaceBetween={30}
         loop={true}
+        keyboard={{
+          enabled: true,
+        }}
         pagination={{
           clickable: true,
         }}
         navigation={true}
-        modules={[Navigation]}
+        modules={[Navigation, Keyboard]}
         className='mySwiper'
       >
         {dummy.map((data, index) => (
-          <>
-            <SwiperSlide>
-              <div key={index}>
-                {/* 포스팅 사진 */}
-                <Image src={dataList[index]} alt='이미지' fill />
+          <SwiperSlide key={data.id}>
+            <div>
+              {/* 포스팅 사진 */}
+              <Image src={dataList[index]} alt='이미지' fill />
 
-                {/* 포스팅 버튼 그룹 */}
-                <div
-                  className='fixed top-40 right-11 h-40 text-4xl text-white flex flex-col justify-between z-50
+              {/* 포스팅 버튼 그룹 */}
+              <div
+                className='fixed top-40 right-11 h-40 text-4xl text-white flex flex-col justify-between z-50
                 max-md:right-3 max-md:text-3xl max-md:justify-around max-md:top-32'
-                >
-                  {/* 포스팅 정보 */}
-                  <button onClick={onClick}>
-                    <motion.div
-                      whileHover={{ scale: 1.1 }}
-                      whileTap={{
-                        scale: 0.97,
-                        opacity: 0.6,
-                      }}
-                    >
-                      <AiOutlineInfoCircle />
-                    </motion.div>
-                  </button>
+              >
+                {/* 포스팅 정보 */}
+                <button onClick={onClick}>
+                  <motion.div
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{
+                      scale: 0.97,
+                      opacity: 0.6,
+                    }}
+                  >
+                    <AiOutlineInfoCircle />
+                  </motion.div>
+                </button>
 
-                  {/* 좋아요 */}
-                  <button onClick={onClick}>
-                    <motion.div
-                      whileHover={{ scale: 1.1 }}
-                      whileTap={{
-                        scale: 0.97,
-                        opacity: 0.6,
-                      }}
-                    >
-                      <FcLikePlaceholder className='svg_color' />
-                    </motion.div>
-                  </button>
+                {/* 좋아요 */}
+                <button onClick={onClick}>
+                  <motion.div
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{
+                      scale: 0.97,
+                      opacity: 0.6,
+                    }}
+                  >
+                    <FcLikePlaceholder className='svg_color' />
+                  </motion.div>
+                </button>
 
-                  {/* 공유하기 */}
-                  <button>
-                    <motion.div
-                      whileHover={{ scale: 1.1 }}
-                      whileTap={{
-                        scale: 0.97,
-                        opacity: 0.6,
-                      }}
-                    >
-                      <BiShareAlt />
-                    </motion.div>
-                  </button>
-                </div>
-
-                {/* 포스팅 내용 */}
-                <div
-                  className='absolute bottom-10 left-10 flex flex-col justify-between  h-44
-                text-white text-left max-md:h-32 max-md:left-0 max-md:bottom-0 max-md:w-full max-md:pl-4 max-md:py-3 max-md:bg-rgba-opacity'
-                >
-                  <p className=''>{data.nickname}님의 저녁</p>
-
-                  <hr className='w-20 h-1 bg-white' />
-
-                  <p>{data.uploadTime} EST</p>
-
-                  <p>
-                    {data.city}, {data.country}
-                  </p>
-
-                  <p>{data.description}</p>
-                </div>
-              </div>
-
-              {/* 스크롤 버튼 */}
-              <div className='absolute bottom-0 m-auto w-full hover:scale-110 transition-all max-md:hidden'>
-                <button onClick={() => moveSectionDown()}>
-                  <Image
-                    src='/images/scroll_icon.png'
-                    alt='Scroll'
-                    width={50}
-                    height={50}
-                  />
+                {/* 공유하기 */}
+                <button>
+                  <motion.div
+                    whileHover={{ scale: 1.1 }}
+                    whileTap={{
+                      scale: 0.97,
+                      opacity: 0.6,
+                    }}
+                  >
+                    <BiShareAlt />
+                  </motion.div>
                 </button>
               </div>
-            </SwiperSlide>
-          </>
+
+              {/* 포스팅 내용 */}
+              <div
+                className='absolute bottom-10 left-10 flex flex-col justify-between  h-44
+                text-white text-left max-md:h-32 max-md:left-0 max-md:bottom-0 max-md:w-full max-md:pl-4 max-md:py-3 max-md:bg-rgba-opacity'
+              >
+                <p className=''>{data.nickname}님의 저녁</p>
+
+                <hr className='w-20 h-1 bg-white' />
+
+                <p>{data.uploadTime} EST</p>
+
+                <p>
+                  {data.city}, {data.country}
+                </p>
+
+                <p>{data.description}</p>
+              </div>
+            </div>
+
+            {/* 스크롤 버튼 */}
+            <div className='absolute bottom-0 m-auto w-full hover:scale-110 transition-all max-md:hidden'>
+              <button onClick={() => moveSectionDown()}>
+                <Image
+                  src='/images/scroll_icon.png'
+                  alt='Scroll'
+                  width={50}
+                  height={50}
+                />
+              </button>
+            </div>
+          </SwiperSlide>
         ))}
       </Swiper>
     </div>
