8becbbe6fce160e2eba296fc84d6f5c3cd24f0c1
JungSik Jeong <wndtlr1024@naver.com>
2023-09-24 18:49:39 +0900

    Style:모바일버전에따라서 글쓰기 버튼 다르게보여주기

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,34 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import AuthModal from '@/components/AuthModal';
 import FullPages from '@/components/FullPages';
 import LoginBtn from '@/components/LoginBtn';
 import PostModal from '@/components/PostModal';
-import { useState } from 'react';
+
+import { HiOutlinePencilSquare } from 'react-icons/hi2';
 
 export default function Home() {
   const [authModal, setAuthModal] = useState<boolean>(false);
   const [postModal, setPostModal] = useState<boolean>(false);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
+
+  const handleResize = () => {
+    if (window.innerWidth <= 768) {
+      setIsMobile(true);
+    } else {
+      setIsMobile(false);
+    }
+  };
+
+  // 모바일 화면인지 체크
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   return (
     <div>
@@ -27,15 +47,27 @@ export default function Home() {
 
       <FullPages />
 
+      {/* TO DO: user접속시에만 보이게하기 */}
       {/* 글쓰기 버튼 */}
-      <footer className='fixed right-16 bottom-14 max-md:hidden'>
-        <button
-          className='text-gray-200 border rounded-md border-gray-200 px-6 py-4 hover:bg-gray-50 hover:text-gray-600 transition-all'
-          onClick={() => setPostModal(true)}
-        >
-          당신의 저녁을 공유해주세요
-        </button>
-      </footer>
+      {isMobile ? (
+        <footer className='fixed right-4 bottom-4'>
+          <button
+            className='text-gray-200 border rounded border-gray-200 px-4 py-3 hover:bg-gray-50 hover:text-gray-600 transition-all'
+            onClick={() => setPostModal(true)}
+          >
+            <HiOutlinePencilSquare size={25} />
+          </button>
+        </footer>
+      ) : (
+        <footer className='fixed right-16 bottom-14'>
+          <button
+            className='text-gray-200 border rounded-md border-gray-200 px-6 py-4 hover:bg-gray-50 hover:text-gray-600 transition-all'
+            onClick={() => setPostModal(true)}
+          >
+            당신의 저녁을 공유해주세요
+          </button>
+        </footer>
+      )}
     </div>
   );
 }
