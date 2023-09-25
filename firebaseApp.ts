83a95cafe097164d73368c3e9cf09b87f572ebf7
JungSik Jeong <wndtlr1024@naver.com>
2023-09-25 11:52:57 +0900

    Feat:파이어베이스 sdk 설정

--- firebaseApp.ts
+++ firebaseApp.ts
@@ -15,9 +15,6 @@ const firebaseConfig = {
   measurementId: process.env.REACT_APP_MEASUREMENTID,
 };
 
-// 이렇게 한 이유는 임폴트를 할때마다 initializeApp하는건 비효율적이라서
-
-// 초기화가 되었으면 초기화된 값을 가져오고, 아니라면 다시 초기화해서 가져오도록 설정
 try {
   // 앱 이름이 제공되면 해당 이름에 해당하는 앱이 반환됩니다
   // 검색 중인 앱이 아직 초기화되지 않은 경우 '예외'가 발생합니다. (공홈 getApp설명)
