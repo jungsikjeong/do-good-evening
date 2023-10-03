c137dc0fee01a7fff6091cfd579363cf7feb5e42
JungSik Jeong <wndtlr1024@naver.com>
2023-10-03 18:33:52 +0900

    Fix: 타입에러

--- recoil/postDetailModalAtoms.tsx
+++ recoil/postDetailModalAtoms.tsx
@@ -1,5 +1,20 @@
-import { PostProps } from '@/app/mypage/page';
 import { atom } from 'recoil';
+
+interface PostProps {
+  content: string;
+  country: string;
+  createdAt: string;
+  email: string;
+  imgUrl: string;
+  id?: string;
+  uid: string;
+  nickname: string;
+  like: likeType[];
+}
+
+type likeType = {
+  likeUser: string;
+};
 
 export const isPostDetailModal = atom<boolean>({
   key: 'isPostDetailModalState',
